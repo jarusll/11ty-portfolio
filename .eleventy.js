d791bca54fb941655ad577ea7a979af59f61eeb7
Suraj Yadav <ysuraj877@gmail.com>
2022-03-04 21:51:59 +0530

    dist -> docs for github hosting

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,7 @@ module.exports = config => {
         htmlTemplateEngine: 'njk',
         dir: {
             input: 'src',
-            output: 'dist'
+            output: 'docs'
         }
     };
 }
