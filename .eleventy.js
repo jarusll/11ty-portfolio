d8b3bb442ada81867310d0287158c75bfaaa4904
Suraj Yadav <ysuraj877@gmail.com>
2022-03-21 13:49:04 +0530

    Add main.css & link in base

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 module.exports = config => {
 
+    config.addPassthroughCopy("./src/main.css");
+
     return {
+	passthroughFileCopy: true,
         markdownTemplateEngine: 'njk',
         dataTemplateEngine: 'njk',
         htmlTemplateEngine: 'njk',
