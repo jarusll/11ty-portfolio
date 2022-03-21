8c1d1a0cc251dd7271295cd90c3bfa9ba57425bd
Suraj Yadav <ysuraj877@gmail.com>
2022-03-21 17:12:31 +0530

    Added footnotes support for markdown

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,22 @@ module.exports = config => {
     config.addCollection("posts", function(collectionApi) {
 	return collectionApi.getFilteredByGlob("./src/posts/*.md");
     });
+
+    // set markdown footnote processor
+    let markdownIt = require("markdown-it");
+    let markdownItFootnote = require("markdown-it-footnote");
     
+    let options = {
+	html: true, // Enable HTML tags in source
+	breaks: true,  // Convert '\n' in paragraphs into <br>
+	linkify: true // Autoconvert URL-like text to links
+    };
+    
+    // configure the library with options
+    let markdownLib =  markdownIt(options).use(markdownItFootnote);
+    // set the library to process markdown files
+    config.setLibrary("md", markdownLib);
+
     return {
 	passthroughFileCopy: true,
         markdownTemplateEngine: 'njk',
