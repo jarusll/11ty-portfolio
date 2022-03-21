b7193ec3db1069c6023257bbf78d1ca1df30cafe
Suraj Yadav <ysuraj877@gmail.com>
2022-03-21 14:21:11 +0530

    Add posts in collections

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,10 @@ module.exports = config => {
 
     config.addPassthroughCopy("./src/main.css");
 
+    config.addCollection("posts", function(collectionApi) {
+	return collectionApi.getFilteredByGlob("./src/posts/*.md");
+    });
+    
     return {
 	passthroughFileCopy: true,
         markdownTemplateEngine: 'njk',
